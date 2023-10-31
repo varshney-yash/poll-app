03b06535962a89de8b91f1b585d32a4671d408d5
Yash Varshney <107148830+varshney-yash@users.noreply.github.com>
2023-10-31 12:00:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,6 @@ import slugify
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-MONGO_URI = uri
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["Polling"]
-users = db["voters"]
-polls = db['polls']
-
 @app.get('/ping')
 async def hello():
     return {'res': 'pong', 'version': __version__, "time": time()}
