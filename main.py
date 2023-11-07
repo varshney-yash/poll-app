bdee1e12dfa6f299b16d3d4bf7bcd00075061881
py3-dev <[200~yash.varshney@careers360.in>
2023-11-07 10:08:33 +0530

    debug print

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI, HTTPException, Request, WebSocket, Depends, status, WebSocketDisconnect
+from fastapi import FastAPI, HTTPException, Request, WebSocket, Depends, Response, WebSocketDisconnect
 from motor.motor_asyncio import AsyncIOMotorClient
 from db import uri
-from models import Poll,Voters,Votes
-from bson import ObjectId
+from models import Poll
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -10,6 +9,11 @@ import slugify
 import asyncio
 from typing import List
 from pydantic import BaseModel
+from uuid import UUID, uuid4
+from fastapi_sessions.backends.implementations import InMemoryBackend
+from fastapi_sessions.session_verifier import SessionVerifier
+from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+
 
 
 app = FastAPI()
@@ -131,5 +135,89 @@ async def websocket_endpoint(websocket: WebSocket):
 
             # Send the updated count to all connected clients
             await manager.send_to_all(str(count))
-    except WebSocketDisconnect:
-        pass 
+    except WebSocketDisconnect as e:
+        print(e)
+
+
+class SessionData(BaseModel):
+    username: str
+
+
+cookie_params = CookieParameters()
+
+# Uses UUID
+cookie = SessionCookie(
+    cookie_name="cookie",
+    identifier="general_verifier",
+    auto_error=True,
+    secret_key="DONOTUSE",
+    cookie_params=cookie_params,
+)
+backend = InMemoryBackend[UUID, SessionData]()
+
+
+class BasicVerifier(SessionVerifier[UUID, SessionData]):
+    def __init__(
+        self,
+        *,
+        identifier: str,
+        auto_error: bool,
+        backend: InMemoryBackend[UUID, SessionData],
+        auth_http_exception: HTTPException,
+    ):
+        self._identifier = identifier
+        self._auto_error = auto_error
+        self._backend = backend
+        self._auth_http_exception = auth_http_exception
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @property
+    def auto_error(self):
+        return self._auto_error
+
+    @property
+    def auth_http_exception(self):
+        return self._auth_http_exception
+
+    def verify_session(self, model: SessionData) -> bool:
+        """If the session exists, it is valid"""
+        return True
+
+
+verifier = BasicVerifier(
+    identifier="general_verifier",
+    auto_error=True,
+    backend=backend,
+    auth_http_exception=HTTPException(status_code=403, detail="invalid session"),
+)
+
+
+@app.post("/create_session/{name}")
+async def create_session(name: str, response: Response):
+
+    session = uuid4()
+    data = SessionData(username=name)
+
+    await backend.create(session, data)
+    cookie.attach_to_response(response, session)
+
+    return f"created session for {name}"
+
+
+@app.get("/whoami", dependencies=[Depends(cookie)])
+async def whoami(session_data: SessionData = Depends(verifier)):
+    return session_data
+
+
+@app.post("/delete_session")
+async def del_session(response: Response, session_id: UUID = Depends(cookie)):
+    await backend.delete(session_id)
+    cookie.delete_from_response(response)
+    return "deleted session"
